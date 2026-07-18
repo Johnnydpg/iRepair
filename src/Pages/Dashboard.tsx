dc3306b39b8ff56e5fd4d42fbdea8c6c26ced476
Johnny De Paula Gomes <johnnydpg2021@gmail.com>
2026-07-18 09:42:31 -0300

    feat(client): Adiciona o cadastramento e listamento de clientes

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,6 +1,6 @@
 const Dashboard = () =>{
     return(
-        <main>
+        <main className="translate-x-140 -translate-y-51 bg-sky-600 w-70 h-20 rounded-md border border-gray-400 shadow-lg text-white text-4xl flex items-center justify-center">
             <h1> Dashboard </h1>
         </main>
     );
--- src/Pages/ServicesOrders.tsx
+++ src/Pages/ServicesOrders.tsx
@@ -1,6 +1,6 @@
 const ServicesOrders = () =>{
     return(
-        <main>
+        <main className="translate-x-140 -translate-y-51 bg-sky-600 w-70 h-20 rounded-md border border-gray-400 shadow-lg text-white text-4xl flex items-center justify-center">
             <h1>Pedidos OS</h1>
         </main>
     );
