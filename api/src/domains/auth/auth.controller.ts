ae5f48c4e2635bfd0be4b562c61527f1fb5c2d61
Johnny De Paula Gomes <johnnydpg2021@gmail.com>
2026-08-12 11:17:33 -0300

    feat(auth): implementa o authController

---
+++ api/src/domains/auth/auth.controller.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { authService } from "./auth.service";
+
+
+class authController{
+    async register(req: Request, res: Response){
+        const{email, password} = req.body;
+        const service = new authService();
+        const user = await service.register(email, password);
+        return res.status(201).json(user);
+    }
+    async login(req: Request, res: Response){
+        const{email, password} = req.body;
+        const service = new authService();
+        const {token, user} = await service.login(email, password);
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: 'lax',
+            maxAge: 60*60*1000,
+        })
+        return res.status(200).json(user);
+    }
+    async logout( req: Request, res: Response){
+        res.clearCookie('token');
+        return res.status(200).json({message: 'Logout realizado com sucesso!'});
+    }
+}
+
