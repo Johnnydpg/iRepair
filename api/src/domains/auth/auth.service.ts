d26c1b69fd8437f4f03d023f4ff9705d89f76163
Johnny De Paula Gomes <johnnydpg2021@gmail.com>
2026-08-12 11:01:10 -0300

    feat(auth): implementa o authService

---
+++ api/src/domains/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import bcrypt from 'bcrypt'
+import { prisma } from "../../config/prismaClients";
+import { generateToken } from '../../utils/tolken';
+import { Apperror } from '../../utils/apperror';
+
+const SALT_ROUNDS = 10;
+
+class authService{
+    async register (email:string, password:string){
+        const existentUser = await prisma.user.findUnique({where: {email},});
+            if(existentUser){
+                throw new Apperror("Email de usuário já existente", 409);
+            }
+        const passwordHash = await bcrypt.hash(password, SALT_ROUNDS);
+        const user = await prisma.user.create({data: {email, password: passwordHash}, select: {id:true, email:true}});
+        return user;
+    }
+    async login(email:string, password:string){
+        const user = await prisma.user.findUnique({where:{email},});
+        if(!user){
+            throw new Apperror("Email ou senha incorretos!", 401);
+        }
+        const correctPassword = await bcrypt.compare(password, user.password);
+        if(!password){
+            throw new Apperror("Email ou senha incorretos!", 401);
+        }
+        const token = generateToken({id:user.id, email: user.email});
+        return {token, user:{id:user.id, email: user.email}};
+    }
+}
+export {authService}
