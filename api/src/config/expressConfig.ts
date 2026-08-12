675096a806d23d417e6c095c77126c5638622751
Johnny De Paula Gomes <johnnydpg2021@gmail.com>
2026-08-12 13:06:26 -0300

    feat(auth): adiciona o middleware

--- api/src/config/expressConfig.ts
+++ api/src/config/expressConfig.ts
@@ -1,9 +1,11 @@
+import cookieParser from "cookie-parser"
 import express from "express"
 import { clientRoutes } from "../domains/clients/clients.routes"
 import { orderRoutes } from "../domains/service-orders/service-order.routes"
 import { authRoutes } from "../domains/auth/auth.routes"
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use('/clients', clientRoutes);
 app.use('/orders', orderRoutes);
--- api/src/domains/clients/clients.routes.ts
+++ api/src/domains/clients/clients.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { clientController } from "./clients.controller";
+import { authController } from "../auth/auth.controller";
+import { authMiddleware } from "../../middlewares/authMiddleware";
 
 const clientRoutes = Router();
 const controller = new clientController();
+clientRoutes.use(authMiddleware);
 
 clientRoutes.post('/', controller.create);
 clientRoutes.get('/', controller.getAll);
