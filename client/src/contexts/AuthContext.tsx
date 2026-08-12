8a17017cb5abd77cdd2ed6c3685b0e6a267396ac
Johnny De Paula Gomes <johnnydpg2021@gmail.com>
2026-08-12 14:19:34 -0300

    feat(authcontext): adiciona a página e a lógica do authcontext

---
+++ client/src/contexts/AuthContext.tsx
@@ -0,0 +1,54 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import type { ReactNode } from "react";
+import { api } from "../services/api";
+
+interface User{
+    id: number
+    email: string
+}
+interface authContextType{
+    user: User | null
+    isAuthenticated: boolean
+    isLoading: boolean
+    login: (email: string, password: string) => Promise<void>
+    logout: ()=> Promise<void>
+}
+
+const authContext = createContext<authContextType | null>(null);
+export function authProvider({children}: {children : ReactNode}){
+    const[user, setUser] = useState<User | null> (null);
+    const[isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+    api.get('/auth/me')
+      .then(res => setUser(res.data.usuario))
+      .catch(() => setUser(null))
+      .finally(() => setIsLoading(false))
+  }, [])
+  async function login(email: string, senha: string) {
+    const response = await api.post('/auth/login', { email, senha })
+    setUser(response.data.user);
+  }
+    async function logout() {
+    await api.post('/auth/logout')
+    setUser(null)
+  }
+
+  return (
+    <authContext.Provider value={{
+      user,
+      isAuthenticated: user !== null,
+      isLoading,
+      login,
+      logout,
+    }}>
+      {children}
+    </authContext.Provider>
+  )
+}
+export function useAuth() {
+  const context = useContext(authContext)
+  if (!context) {
+    throw new Error('useAuth deve ser usado dentro de um AuthProvider')
+  }
+  return context
+}
